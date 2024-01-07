921ce930333abc3cd60edb8cc0393872f9ae39f7
iabrar <abrar.ksu@gmail.com>
2024-01-07 20:41:55 +0300

    [feat] create ingredient model migration file

---
+++ src/database/migrations/2024_01_07_171019_create_ingredients_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Ingredient;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('stock_quantity', 8, 2);
+            $table->string('unit')->default(Ingredient::UNIT_GRAMS)->change();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
