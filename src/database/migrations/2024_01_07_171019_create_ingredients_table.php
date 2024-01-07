c5ab55793e21ba5244f2456e59ec5d0044001c7f
iabrar <abrar.ksu@gmail.com>
2024-01-07 21:49:55 +0300

    [feat] remove the change from the file

--- src/database/migrations/2024_01_07_171019_create_ingredients_table.php
+++ src/database/migrations/2024_01_07_171019_create_ingredients_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->decimal('stock_quantity', 8, 2);
-            $table->string('unit')->default(Ingredient::UNIT_GRAMS)->change();
+            $table->string('unit')->default(Ingredient::UNIT_GRAMS);
             $table->timestamps();
         });
     }
