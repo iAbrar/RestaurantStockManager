816041ecaab8aca00498551ef9a22b553c60ad63
iabrar <abrar.ksu@gmail.com>
2024-01-13 15:31:30 +0300

    [modify] the seeder to get the initial data from config file

--- src/database/seeders/IngredientSeeder.php
+++ src/database/seeders/IngredientSeeder.php
@@ -10,9 +10,9 @@ class IngredientSeeder extends Seeder
     public function run()
     {
         $ingredients = [
-            ['name' => 'Beef',   'stock_amount' => 20000, 'unit' => 'g'], // 20kg in grams
-            ['name' => 'Cheese', 'stock_amount' => 5000,  'unit' => 'g'], // 5kg in grams
-            ['name' => 'Onion',  'stock_amount' => 1000,  'unit' => 'g'], // 1kg in grams
+            ['name' => 'Beef', 'stock_amount' => config('ingredients.initial_stocks.Beef'), 'unit' => 'g'], // 20kg in grams
+            ['name' => 'Cheese', 'stock_amount' => config('ingredients.initial_stocks.Cheese'), 'unit' => 'g'], // 5kg in grams
+            ['name' => 'Onion', 'stock_amount' => config('ingredients.initial_stocks.Onion'), 'unit' => 'g'], // 1kg in grams
         ];
 
         foreach ($ingredients as $ingredient) {
